79ea013560133e7edfdc672a38c78273b3c944d6
nese <ilknurnese@gmail.com>
2024-10-12 13:35:55 +0300

    Q4 and solution added  en

---
+++ src/main/Java/Q004/Q004.java
@@ -0,0 +1,49 @@
+package main.Java.Q004;
+
+public class Q004 {
+    public static void main(String[] args) {
+
+
+        int[] array = {10, 20, 30};
+        int size = 3;
+        int index = 0;
+
+        /* line n1 */
+
+        System.out.print("The top element: " + array[index]);
+
+    }
+}
+
+
+/*
+
+Given the code fragment:
+
+Which code fragments, inserted at line n1 independently, print The Top element: 30? (Choose two.)
+
+ * A. 		do {
+			index++;
+		}while(index >= size);
+
+	B.
+			while(index < size-1){
+			index++;
+		}
+
+	C. 	do {
+			index++;
+		}while(index < size-1);
+
+	D. 	do {
+			index++;
+		}while(index <= size);
+
+
+	E.	while(index <= size -1){
+			index++;
+		}
+ */
+
+
+
