5671c8cfba36f648fd0c78619db4e13f7089858d
nese <ilknurnese@gmail.com>
2024-10-20 18:58:20 +0300

    Q5 added

---
+++ src/main/Java/Q005/Q005.java
@@ -0,0 +1,20 @@
+package main.Java.Q005;
+
+public class Q005 {
+    public static void main(String[] args) {
+
+/*
+
+            Which statement is true about the switch statement?
+
+            A. It must contain the default section.
+            B. The break statement, at the end of each case block, is optional.
+            C. Its case label literals can be changed at runtime.
+            D. Its expression must evaluate to a collection of values.
+
+
+ */
+
+
+    }
+}
